db7162f5d8b31b648546d14a6e00703120e92a54
Luis Condori Dell 5593 <lmcondori@outlook.com>
2022-11-15 03:52:32 -0500

    Agregar campo rol en la migracion

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->char('role',1)->comment('A:admin | P:profesor');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
